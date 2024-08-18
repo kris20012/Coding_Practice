1a894a8948c8386047e699575ab9916da805ee8b
Krishanth Suthaharan <krishanth2001@live.com>
2024-08-18 18:04:54 -0400

    Completed linked list and node classes with private and public membering

--- reversePrefitx.cpp
+++ reversePrefitx.cpp
@@ -8,13 +8,16 @@ using namespace std;
 
 class listNode {
     private:
+        //char key;
+        //listNode* next;
+    public:
         char key;
         listNode* next;
-    public:
         listNode(char c);
         char getKey() const;
         listNode* getNext() const;
         void setKey(char c);
+        void setNext(listNode* n);
         void print() const;
 };
 
@@ -31,6 +34,10 @@ listNode* listNode::getNext() const{
     return next;
 }
 
+void listNode::setNext(listNode* n) {
+    next = n;
+}
+
 void listNode::setKey(char c){
     key = c;
 }
@@ -45,29 +52,66 @@ class linkedList{
     public:
         linkedList();
         void insertNode(char c);
-        void travel(char stop);
+        void printRev();
 };
 
 linkedList::linkedList(){
     head = nullptr;
 }
 
-linkedList::insertNode(char c){
+void linkedList::insertNode(char c){
     listNode* newNode = new listNode(c);
 
     // if empty
     if(head == nullptr){
         head = newNode;
-        return;
+    } else {
+        listNode* tmp = head;
+        while(tmp->getNext() != nullptr){
+            tmp = tmp->getNext();
+        }
+        tmp->setNext(newNode);
     }
+}
 
+void linkedList::printRev(){
     listNode* tmp = head;
-    while(tmp->getNext != nullptr){
-        tmp = tmp->getNext;
-    }
+    cout << tmp << endl;
+    cout << tmp->getNext();
+    cout << "\nTrack\n";
+    cout << tmp << endl;
+    cout << tmp->getNext() << endl;
+    cout << tmp->next->next << endl;
+    cout << tmp->next->next->next << endl;
+    while(tmp!=nullptr){
+        //cout << tmp->getKey();
+        cout << tmp << endl;
+        cout << tmp->getNext() << endl;
 
+        tmp->setNext(tmp->getNext());
+        cout << "SECOND: " << tmp->getNext()<< endl;
+        cout << "THIRD: " << tmp->next<< endl;
+    }
 }
 
 string reversePrefix(string word, char ch) {
-        
+    linkedList reverse;
+    bool chFound = false;
+    for(int i = 0; i < word.size(); i++){
+        cout << "HERE\n";
+        if(word[i] != ch && !chFound){
+            reverse.insertNode(word[i]);
+        } else {
+            if(!chFound) {
+                reverse.printRev();
+            }
+            chFound = true;
+            cout << word[i];
+        }
     }
+}
+
+int main (){
+    reversePrefix("abcdefd", 'd');
+    return 0;
+}
