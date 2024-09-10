d6e19c2347d9a3c752d27679a22d0acc29cf27df
Krishanth Suthaharan <krishanth2001@live.com>
2024-09-09 20:15:40 -0400

    Failing time complexity now

--- prefixDivBy5.cpp
+++ prefixDivBy5.cpp
@@ -41,16 +41,42 @@ vector<bool> prefixesDivBy5(vector<int>& nums) {
         return tmp;
     }
 
+vector<bool> prefixesDivBy5_v2(vector<int>& nums) {
+        vector<bool> tmp;
+        int sum = nums[0];
+        if(sum % 5 == 0){
+                tmp.push_back(true);
+            } else {
+                tmp.push_back(false);
+            }
+        
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == 0){
+                sum += sum * 2;
+            } else {
+                sum += sum * 2 + 1;
+            }
+            if(sum % 5 == 0){
+                tmp.push_back(true);
+            } else {
+                tmp.push_back(false);
+            }
+            cout << "SUM: " << sum << endl;
+        }
+    
+        return tmp;
+    }
+
 int main() {
     // 0, 1, and 3
     vector <int> num = {0,1,1};
     cout << "ANSWER: \n";
-    printArr(prefixesDivBy5(num));
+    printArr(prefixesDivBy5_v3(num));
 
     // 1, 3, and 7
     num = {1,1,1};
     cout << "ANSWER: \n";
-    printArr(prefixesDivBy5(num));
+    printArr(prefixesDivBy5_v3(num));
 
     return 0;
 }
