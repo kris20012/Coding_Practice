2e094ea5c555531fcec467c1ed4bbebdf77447f2
Krishanth Suthaharan <krishanth2001@live.com>
2024-10-17 07:40:25 -0400

    Naive O(n^2) algorithm fails timing

---
+++ containerWithMostWater.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int calculateArea(int start, int end, int width){
+if(start < end){
+    return start * width;
+} else {
+    return end * width;
+}
+}
+
+int maxArea(vector<int>& height) {
+    int max = 0;
+    int area = 0;
+    int width = 0;
+    int left = 0;
+    int right = 0;
+
+    // Naive approach doesn't fit run time
+    for(int i = 0; i < height.size(); i++){
+        for(int j = i + 1; j < height.size(); j++){
+            cout << "I: " << height[i] << "     J: " << height[j] << endl;
+            width = i < j ? j - i : i - j;
+            area = calculateArea(height[i], height[j], width);
+            if(area > max) max = area;
+        }
+    }
+
+    return max;
+
+}
